7c8463877caebbca6e91594eb39154245ee441af
Akshay Pai <akshaypaik@gmail.com>
2025-03-19 16:29:12 +0530

    watch page skeleton component added

--- src/utils/constants/apiConstants.js
+++ src/utils/constants/apiConstants.js
@@ -1,6 +1,6 @@
 const YOUTUEBE_API_KEY = "";
 
-export const YOUTUBE_MOST_POPULAR_VIDEO = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&maxResults=50&key=${YOUTUEBE_API_KEY}`; 
+export const YOUTUBE_MOST_POPULAR_VIDEO = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key=${YOUTUEBE_API_KEY}`; 
 
 
 
