ec4ba89cd832badecdd9e1765a9740706ae17398
Akshay Pai <akshaypaik@gmail.com>
2025-03-19 18:04:24 +0530

    adding key constant file

--- src/utils/constants/apiConstants.js
+++ src/utils/constants/apiConstants.js
@@ -1,6 +1,6 @@
-const YOUTUEBE_API_KEY = "";
+import { YOUTUBE_API_KEY } from "./keyConstants";
 
-export const YOUTUBE_MOST_POPULAR_VIDEO = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key=${YOUTUEBE_API_KEY}`; 
+export const YOUTUBE_MOST_POPULAR_VIDEO = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key=${YOUTUBE_API_KEY}`; 
 
 
 
